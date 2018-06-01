15d684e3d639f81b68632ace4eb920ddf2db9ac7
Wyzards <mcwyzards@gmail.com>
2018-06-01 07:51:25 -0400

    Update main.js
    Still trying to fix going off screen. Doing commits in class only way to test is rawgit so more commits.

--- main.js
+++ main.js
@@ -289,7 +289,7 @@ function gameLoop() {
 		//if 4200 >= 4200
 	} else if (piece.x >= gameArea.worldW-piece.width-(gameArea.canvas.width/2)) {
 		//this x val doesnt work and causes error?
-		translatedX = gameArea.worldW-gameArea.canvas.width-piece.width;
+		translatedX = gameArea.worldW-gameArea.canvas.width;
 		//can go 100 past world barrier?
 	} else {
 		translatedX = piece.x-(gameArea.canvas.width/2);
